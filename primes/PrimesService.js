1c90ad0d0c0339c0906e317a0f285956c085868b
Renan Fernandes Lorenzetti <42356454+lolorenzetti@users.noreply.github.com>
2018-08-21 18:43:46 -0300

    Update PrimesService.js

--- primes/PrimesService.js
+++ primes/PrimesService.js
@@ -1,6 +1,5 @@
 const isPrime = function (number) {
-    if (number === 0) return false;
-    if (number === 1) return false;
+    if (number === 0 || number === 0) return false;
     let divisor = 2;
     while (divisor < number) {
         if (number%divisor === 0) return false;
